51c95f76743132ad8351f5cb0e3d89e151e0f1c1
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2021-05-13 23:58:45 -0300

    setting config

--- weblogin/settings.py
+++ weblogin/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django.contrib.auth
 import django_messages
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -174,3 +175,4 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 #AUTH_USER_MODEL= django.contrib.auth.get_user_model()
 
 FORM_RENDERER = 'django.forms.renderers.DjangoTemplates'
+django_heroku.settings(locals())
